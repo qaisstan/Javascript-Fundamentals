96f0a76c3ec76c143deadb40c6d8d016d690ae98
qaisstan <94216834+qaisstan@users.noreply.github.com>
2024-10-22 16:56:40 +0200

    boolean

---
+++ string.js
@@ -0,0 +1,18 @@
+// Creating a single-line string
+
+let firstName = "Qais";
+let lastName = "Stanikzai";
+let title = `CEO`;
+
+// String concatenation with the plus operator
+let fullName = firstName + " " + lastName;
+
+// string concatenation with template literals
+fullName = `${firstName} ${lastName}`;
+console.log(fullName);
+
+// Creating multiline line strings with \n
+let bio = "Line 1\nLine 2\nLine 3 ";
+console.log(bio);
+
+// Creating multiline strings with backticks
